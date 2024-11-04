3636d545c6a6fdcfa023d1f6dacacf7b1e9e7098
Luna <62033805+Luna5ama@users.noreply.github.com>
2024-11-04 12:03:56 -0500

    Temp colorTex

--- shaders/base/TexFormats.glsl
+++ shaders/base/TexFormats.glsl
@@ -2,9 +2,15 @@
 const int colortex0Format = RGBA16F;
 const int colortex1Format = RGBA32UI;
 const int colortex2Format = R32F;
+const int colortex3Format = RGBA16F;
+const int colortex4Format = RGBA16F;
+const int colortex5Format = RGBA32UI;
 const int colortex15Format = RGBA16F;
 */
 
 #define usam_gbuffer colortex1
 #define usam_viewZ colortex2
+#define usam_16FTemp2 colortex3
+#define usam_16FTemp3 colortex4
+#define usam_32UITemp colortex5
 #define usam_taaLast colortex15
