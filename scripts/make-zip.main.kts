e5a3c5c29183862cf538e9551b25d512600db8de
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-02-03 03:02:17 -0500

    Fixed make zip

--- scripts/make-zip.main.kts
+++ scripts/make-zip.main.kts
@@ -1,5 +1,6 @@
 import java.io.File
 import java.util.zip.Deflater
+import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
 val excluded = setOf("scripts", ".*\\.zip").map { it.toRegex() }
@@ -7,12 +8,11 @@ val excluded = setOf("scripts", ".*\\.zip").map { it.toRegex() }
 val currDir = File("")
 val rootDir = currDir.absoluteFile.parentFile
 ZipOutputStream(File(rootDir, "${rootDir.name}.zip").outputStream(), Charsets.UTF_8).use { zipOut ->
-    zipOut.setLevel(Deflater.BEST_COMPRESSION)
+    zipOut.setLevel(Deflater.DEFAULT_COMPRESSION)
     zipOut.setMethod(ZipOutputStream.DEFLATED)
 
     rootDir.walk().asSequence()
         .filter { file ->
-            if (file.isDirectory) return@filter false
             val baseDirName = file.relativeTo(rootDir).path.substringBefore(File.separator)
             if (baseDirName.startsWith('.')) return@filter false
             if (excluded.any { it.matches(baseDirName) }) return@filter false
@@ -20,8 +20,13 @@ ZipOutputStream(File(rootDir, "${rootDir.name}.zip").outputStream(), Charsets.UT
             return@filter true
         }
         .forEach { file ->
-            val relativePath = file.relativeTo(rootDir)
-            zipOut.putNextEntry(java.util.zip.ZipEntry(relativePath.toString()))
+            val relativePath = file.relativeTo(rootDir).invariantSeparatorsPath
+            if (file.isDirectory) {
+                zipOut.putNextEntry(ZipEntry("$relativePath/"))
+                zipOut.closeEntry()
+                return@forEach
+            }
+            zipOut.putNextEntry(ZipEntry(relativePath.toString()))
             file.inputStream().use { input ->
                 input.copyTo(zipOut)
             }
