2cb3029527b9db5d74e57c403ac8446627d55c52
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-03-30 16:34:56 -0400

    Denoiser optimize

--- shaders/denoiser/Update.glsl
+++ shaders/denoiser/Update.glsl
@@ -11,8 +11,8 @@ void gi_update(vec3 currColor, vec4 prevColorHLen, vec2 prevMoments, out float n
         newMoments = currMoments;
         filterInput.rgb = currColor;
     } else {
-        newHLen = min(prevColorHLen.a + 1.0, SETTING_DENOISER_MAX_ACCUM);
-        float alpha = 1.0 / pow(newHLen, SETTING_DENOISER_ACCUM_DECAY);
+        newHLen = min(prevColorHLen.a + 1.0, 1024.0);
+        float alpha = 1.0 / pow(min(newHLen, SETTING_DENOISER_MAX_ACCUM), SETTING_DENOISER_ACCUM_DECAY);
         newMoments = mix(prevMoments, currMoments, alpha);
         filterInput.rgb = mix(prevColorHLen.rgb, currColor, alpha);
     }
