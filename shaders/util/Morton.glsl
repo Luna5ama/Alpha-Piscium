64ebb23bd1bd3e10becf0ac15d7fc551436bd371
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-05-25 05:16:44 -0400

    Morton code source link

--- shaders/util/Morton.glsl
+++ shaders/util/Morton.glsl
@@ -2,6 +2,7 @@
 #define INCLUDE_util_Morton_glsl a
 #include "/_Base.glsl"
 
+// Adapted from https://gist.github.com/JarkkoPFC/0e4e599320b0cc7ea92df45fb416d79a
 uvec2 morton_8bDecode(uint code) {
     uvec2 result = uvec2(code, code >> 1) & 0x55u;
     result = (result | (result >> 1)) & 0x33u;
