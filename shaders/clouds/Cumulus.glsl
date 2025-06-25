e9b2cf89a22cbc847d364e95a5aa6ff3c1e5e278
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-06-25 18:49:44 -0400

    CU tweak again

--- shaders/clouds/Cumulus.glsl
+++ shaders/clouds/Cumulus.glsl
@@ -25,8 +25,8 @@ bool clouds_cu_density(vec3 rayPos, float heightFraction, out float densityOut) 
     shapeParams.octaveCount = 4u;
     mat2 rotationMatrix = mat2_rotate(GOLDEN_ANGLE);
     float coverage = GradientNoise_3D_value_fbm(shapeParams, rayPos + vec3(0.0, 12.0, 0.0));
-    const float CUMULUS_FACTOR = 0.7;
-    const float _CU_COVERAGE_FACTOR = 1.0 - (pow(SETTING_CLOUDS_CU_COVERAGE, 0.3 + CUMULUS_FACTOR * 1.5));
+    const float CUMULUS_FACTOR = 0.8;
+    const float _CU_COVERAGE_FACTOR = 1.0 - (pow(SETTING_CLOUDS_CU_COVERAGE, 0.3 + CUMULUS_FACTOR * 1.0));
     const float SIGMOID_K = mix(0.2, 4.0, pow2(CUMULUS_FACTOR));
     coverage = rcp(1.0 + exp2(-coverage * SIGMOID_K)); // Sigmoid
     coverage = linearStep(_CU_COVERAGE_FACTOR, 1.0, coverage);
@@ -54,39 +54,39 @@ bool clouds_cu_density(vec3 rayPos, float heightFraction, out float densityOut) 
 
     if (base > _CU_DENSITY_EPSILON) {
         FBMParameters curlParams;
-        curlParams.frequency = 0.3;
+        curlParams.frequency = 0.2;
         curlParams.persistence = -0.8;
         curlParams.lacunarity = 2.6;
         curlParams.octaveCount = 2u;
         vec2 curl2D = GradientNoise_2D_grad_fbm(curlParams, rotationMatrix, rayPos.xz);
         vec3 curl = vec3(curl2D.x, 0.0, curl2D.y);
-        curl *= 0.5 + 1.0 * heightFraction;
+        curl *= 0.5 + 0.5 * heightFraction;
 
         densityOut = base;
 
         // Carve out basic cloud shape
         FBMParameters densityParams;
-        densityParams.frequency = 2.4;
+        densityParams.frequency = 1.4;
         densityParams.persistence = -0.65;
-        densityParams.lacunarity = 2.1;
+        densityParams.lacunarity = 2.2;
         densityParams.octaveCount = 2u;
-        float detail1 = GradientNoise_3D_value_fbm(densityParams, rayPos + curl * 0.4) * 2.0;
+        float detail1 = GradientNoise_3D_value_fbm(densityParams, rayPos + curl * 0.4) * 3.0;
         detail1 = smoothstep(-1.0, 1.0, detail1);
-        detail1 *= mix(0.5, 0.8, heightFraction);
-        detail1 *= pow2(CUMULUS_FACTOR) * 0.95 + 0.05;
+        detail1 *= mix(0.6, 0.7, heightFraction);
+        detail1 *= CUMULUS_FACTOR * 0.95 + 0.05;
         densityOut = linearStep(saturate(detail1), 1.0, densityOut);
 
         if (densityOut > _CU_DENSITY_EPSILON) {
             // Add some high frequency detail to edges
             FBMParameters valueNoiseParams;
-            valueNoiseParams.frequency = 9.2;
+            valueNoiseParams.frequency = 6.9;
             valueNoiseParams.persistence = 0.7;
             valueNoiseParams.lacunarity = 3.1;
             valueNoiseParams.octaveCount = 2u;
-            float detail2 = GradientNoise_3D_value_fbm(valueNoiseParams, rayPos + curl * -0.2);
-            detail2 = mix(saturate(detail2 * 0.5 + 0.5), abs(detail2), heightFraction * 0.6);
-            detail2 *= mix(0.8, 1.0, heightFraction);
-            detail2 *= pow2(CUMULUS_FACTOR) * 0.95 + 0.05;
+            float detail2 = GradientNoise_3D_value_fbm(valueNoiseParams, rayPos + curl * -0.4) * 2.0;
+            detail2 = mix(saturate(detail2 * 0.5 + 0.5), abs(detail2), heightFraction * 0.4);
+            detail2 *= mix(0.4, 0.6, heightFraction);
+            detail2 *= CUMULUS_FACTOR * 0.95 + 0.05;
             densityOut = linearStep(saturate(detail2), 1.0, densityOut);
 
             densityOut *= 1.0 - heightFraction;
