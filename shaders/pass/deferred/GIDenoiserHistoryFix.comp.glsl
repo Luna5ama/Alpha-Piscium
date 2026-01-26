5b99fe8a3a1f5ba84294c01b8432ea5ff19c5ca5
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-01-26 01:36:04 -0500

    GI history length filter tweak

--- shaders/pass/deferred/GIDenoiserHistoryFix.comp.glsl
+++ shaders/pass/deferred/GIDenoiserHistoryFix.comp.glsl
@@ -13,6 +13,8 @@
 layout(local_size_x = 16, local_size_y = 16) in;
 const vec2 workGroupsRender = vec2(1.0, 1.0);
 
+layout(rgba16f) uniform writeonly image2D uimg_temp1;
+layout(rgba16f) uniform writeonly image2D uimg_temp2;
 layout(rgba16f) uniform writeonly image2D uimg_temp3;
 layout(rgba16f) uniform writeonly image2D uimg_rgba16f;
 layout(rgba8) uniform writeonly image2D uimg_rgba8;
