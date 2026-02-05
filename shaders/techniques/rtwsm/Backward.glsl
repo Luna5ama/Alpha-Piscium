4817ac3f4f1f4d94a18a74b3cb0a3e521997feb8
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-02-05 15:20:37 -0500

    Fixed backward analysis distance check

--- shaders/techniques/rtwsm/Backward.glsl
+++ shaders/techniques/rtwsm/Backward.glsl
@@ -50,14 +50,15 @@ void shadowAABB2() {
 }
 
 void importance(ivec2 texelPos, float viewZ, GBufferData gData, out uint p, out float v) {
-    vec2 screenPos = (vec2(texelPos) + 0.5 - global_taaJitter) * uval_mainImageSizeRcp;
+    vec2 screenPos = coords_texelToUV(texelPos, uval_mainImageSizeRcp);
     vec3 viewPos = coords_toViewCoord(screenPos, viewZ, global_camProjInverse);
+    vec4 scenePos = gbufferModelViewInverse * vec4(viewPos, 1.0);
+
     float importance = SETTING_RTWSM_B_BASE;
-    if (dot(viewPos, viewPos) > shadowDistance * shadowDistance) {
+    if (lengthSq(scenePos.xz) > pow2(shadowDistance)) {
         viewPos = vec3(0.0);
         importance = 0.0;
     }
-    vec4 scenePos = gbufferModelViewInverse * vec4(viewPos, 1.0);
     vec4 shadowViewPos = global_shadowRotationMatrix * shadowModelView * scenePos;
     shadowAABB1(shadowViewPos.xyz);
 
