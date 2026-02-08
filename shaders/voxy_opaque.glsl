f3f5715ddafc6f40496a3854a653a04870389c51
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-02-08 18:38:54 -0500

    Cleanup lmCoord in voxy pass

--- shaders/voxy_opaque.glsl
+++ shaders/voxy_opaque.glsl
@@ -6,21 +6,22 @@ layout(location = 0) out uvec4 rt_gbufferData;
 layout(location = 1) out vec4 rt_test;
 
 void voxy_emitFragment(VoxyFragmentParameters parameters) {
-    vec3 base_color = parameters.sampledColour.rgb * parameters.tinting.rgb;
+    vec3 color = parameters.sampledColour.rgb * parameters.tinting.rgb;
+    uint materialID = parameters.customId & 0xFFFFu;
 
     // R: UV (2x16 unorm)
     uint packedUV = packUnorm2x16(parameters.uv);
 
     // G: Color (3x8 unorm) + Face (3 bits in alpha slot)
-    uint packedColorFace = packUnorm4x8(vec4(base_color, 0.0));
+    uint packedColorFace = packUnorm4x8(vec4(color, 0.0));
     packedColorFace = bitfieldInsert(packedColorFace, parameters.face, 24, 3);
 
     // B: Lightmap (2x8 unorm) + MaterialID (16 bits)
-    uint lmx = uint(clamp(parameters.lightMap.x, 0.0, 1.0) * 255.0);
-    uint lmy = uint(clamp(parameters.lightMap.y, 0.0, 1.0) * 255.0);
-    uint lmPacked = lmx | (lmy << 8);
-    uint matID = parameters.customId & 0xFFFFu;
-    uint packedLMMat = lmPacked | (matID << 16);
+    vec2 lmCoord = parameters.lightMap;
+    lmCoord.x = linearStep(0.0625, 0.96875, lmCoord.x);
+    lmCoord.y = linearStep(0.125, 0.73438, lmCoord.y);
+    uint lmPacked = packUnorm4x8(vec4(lmCoord, 0.0, 0.0));
+    uint packedLMMat = bitfieldInsert(lmPacked, materialID, 16, 16);
 
     // A: ViewZ (float as uint)
     float viewZ = -rcp(gl_FragCoord.w);
--- shaders/voxy_translucent.glsl
+++ shaders/voxy_translucent.glsl
@@ -8,20 +8,30 @@ layout(location = 0) out uvec4 rt_gbufferData;
 layout(location = 1) out vec4 rt_translucentColor;
 
 void voxy_emitFragment(VoxyFragmentParameters parameters) {
-
-    vec4 albedo = parameters.sampledColour * parameters.tinting;
+    vec3 color = parameters.sampledColour.rgb * parameters.tinting.rgb;
     uint materialID = parameters.customId & 0xFFFFu;
-    vec3 materialColor = colors2_material_toWorkSpace(albedo.rgb);
-    vec4 transmittanceV = translucent_albedoToTransmittance(materialColor, albedo.a, materialID);
+
+    vec3 materialColor = colors2_material_toWorkSpace(color.rgb);
+    vec4 transmittanceV = translucent_albedoToTransmittance(materialColor, alpha, materialID);
     rt_translucentColor = transmittanceV;
 
+    // R: UV (2x16 unorm)
     uint packedUV = packUnorm2x16(parameters.uv);
-    uint packedColorFace = packUnorm4x8(vec4(albedo.rgb, float(parameters.face) / 255.0));
-    uint lmx = uint(clamp(parameters.lightMap.x, 0.0, 1.0) * 255.0);
-    uint lmy = uint(clamp(parameters.lightMap.y, 0.0, 1.0) * 255.0);
-    uint lmPacked = lmx | (lmy << 8);
-    uint packedLMMat = lmPacked | (materialID << 16);
 
+    // G: Color (3x8 unorm) + Face (3 bits in alpha slot)
+    uint packedColorFace = packUnorm4x8(vec4(color, 0.0));
+    packedColorFace = bitfieldInsert(packedColorFace, parameters.face, 24, 3);
+
+    // B: Lightmap (2x8 unorm) + MaterialID (16 bits)
+    vec2 lmCoord = parameters.lightMap;
+    lmCoord.x = linearStep(0.0625, 0.96875, lmCoord.x);
+    lmCoord.y = linearStep(0.125, 0.73438, lmCoord.y);
+    uint lmPacked = packUnorm4x8(vec4(lmCoord, 0.0, 0.0));
+    uint packedLMMat = bitfieldInsert(lmPacked, materialID, 16, 16);
+
+    // A: ViewZ (float as uint)
     float viewZ = -rcp(gl_FragCoord.w);
+
     rt_gbufferData = uvec4(packedUV, packedColorFace, packedLMMat, floatBitsToUint(viewZ));
+    rt_test = gl_FragCoord;
 }
