d4c5a2b9bcd7d837d1ad72cbaa50feed4f39da41
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-04-04 05:37:40 -0400

    Re-enable MB

--- shaders/pass/deferred/DirectLighting.glsl
+++ shaders/pass/deferred/DirectLighting.glsl
@@ -106,7 +106,7 @@ void main() {
             vec4 giOut1 = vec4(0.0);
             vec4 giOut2 = vec4(0.0);
 
-//            giOut1.rgb = transient_gi2Reprojected_fetch(texelPos).rgb;
+            giOut1.rgb = transient_gi1Reprojected_fetch(texelPos).rgb;
 
             vec4 mainOut = vec4(0.0, 0.0, 0.0, 1.0);
             if (lighting_gData.materialID == 65534u) {
