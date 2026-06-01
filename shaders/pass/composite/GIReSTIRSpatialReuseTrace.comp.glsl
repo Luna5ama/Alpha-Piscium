33d79f0dafebb5477d1cffc5769145cb518755b2
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-06-01 03:26:33 -0400

    Fixed spatial reuse continue trace specular result discard

--- shaders/pass/composite/GIReSTIRSpatialReuseTrace.comp.glsl
+++ shaders/pass/composite/GIReSTIRSpatialReuseTrace.comp.glsl
@@ -21,6 +21,7 @@ void handleRayResult(SSTRay sstRay) {
 
     if (discardSptialReuse) {
         transient_ssgiDiffOut_store(texelPos, vec4(0.0));
+        transient_ssgiSpecOut_store(texelPos, vec4(0.0));
         #if SETTING_DEBUG_OUTPUT
         imageStore(uimg_temp5, texelPos, vec4(1.0, 0.0, 0.0, 0.0));
         #endif
