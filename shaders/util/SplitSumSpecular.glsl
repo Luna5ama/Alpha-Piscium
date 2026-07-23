c8364f4b33b8f00ef1c4a1317502ac2b2959affb
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-07-22 22:55:51 -0700

    Really Fix specular overflow

--- shaders/util/SplitSumSpecular.glsl
+++ shaders/util/SplitSumSpecular.glsl
@@ -12,27 +12,11 @@ vec3 splitSumSpecularLUT(float F0, float NDotV, float roughness) {
     return splitSumSpecularLUT(vec3(F0), vec3(1.0), NDotV, roughness);
 }
 
-#ifndef GI_SPEC_DENOISE_MIN_FACTOR
-#define GI_SPEC_DENOISE_MIN_FACTOR 0.02
-#endif
-
-#ifndef GI_SPEC_DENOISE_ROUGHNESS_START
-#define GI_SPEC_DENOISE_ROUGHNESS_START 0.02
-#endif
-
-#ifndef GI_SPEC_DENOISE_ROUGHNESS_END
-#define GI_SPEC_DENOISE_ROUGHNESS_END 0.08
-#endif
+const float GI_SPEC_DENOISE_MIN_FACTOR = 0.05;
 
 vec3 splitSumSpecularDenoiseFactor(vec3 F0, vec3 F82Tint, float NDotV, float roughness) {
     vec3 physicalAlbedo = splitSumSpecularLUT(F0, F82Tint, NDotV, roughness);
-    vec3 liftedFactor = mix(vec3(GI_SPEC_DENOISE_MIN_FACTOR), vec3(1.0), physicalAlbedo);
-    float demodulationAmount = smoothstep(
-        GI_SPEC_DENOISE_ROUGHNESS_START,
-        GI_SPEC_DENOISE_ROUGHNESS_END,
-        roughness
-    );
-    return mix(vec3(1.0), liftedFactor, demodulationAmount);
+    return mix(vec3(GI_SPEC_DENOISE_MIN_FACTOR), vec3(1.0), physicalAlbedo);
 }
 
 vec3 splitSumSpecularDenoiseFactor(float F0, float NDotV, float roughness) {
