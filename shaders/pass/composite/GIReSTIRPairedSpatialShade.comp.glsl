3703812d6a535af05036e9b681978c98720e941b
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-08-09 03:54:45 -0700

    Defer paired spatial block model decode

--- shaders/pass/composite/GIReSTIRPairedSpatialShade.comp.glsl
+++ shaders/pass/composite/GIReSTIRPairedSpatialShade.comp.glsl
@@ -11,6 +11,7 @@ layout(local_size_x = 16, local_size_y = 16) in;
 #include "/techniques/HiZCheck.glsl"
 #include "/techniques/gi/PairwiseMISMetadata.glsl"
 #define VOXEL_BLOCK_MODEL_TRIPLE_PRODUCT
+#define VOXEL_TRACE_DEFER_BLOCK_MODEL_DECODE
 #include "/techniques/voxel/VoxelTrace.glsl"
 
 const vec2 workGroupsRender = vec2(1.0, 1.0);
