90efb3d9188f4637646f57577a1d94a89e0ee0a3
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-08-20 17:44:29 -0700

    Split spatial Morton lookup planes

--- shaders/techniques/voxel/VoxelTrace.glsl
+++ shaders/techniques/voxel/VoxelTrace.glsl
@@ -67,7 +67,9 @@ uint _voxel_spreadBits(uint x) {
 shared uint _voxel_levelOffsets[6];
 shared ivec2 _voxel_levelSizeMask[6];
 #ifdef INCLUDE_techniques_gi_PairwiseMISMetadata_glsl
-shared uvec3 _voxel_spreadLUT[VOXEL_GRID_SIZE * VOXEL_BRICK_SIZE];
+shared uint _voxel_spreadLUTX[VOXEL_GRID_SIZE * VOXEL_BRICK_SIZE];
+shared uint _voxel_spreadLUTY[VOXEL_GRID_SIZE * VOXEL_BRICK_SIZE];
+shared uint _voxel_spreadLUTZ[VOXEL_GRID_SIZE * VOXEL_BRICK_SIZE];
 #else
 shared uint _voxel_spreadLUT[VOXEL_GRID_SIZE * VOXEL_BRICK_SIZE];
 #endif
@@ -76,9 +78,9 @@ shared vec3 _voxel_gridOriginF;
 uint _voxel_packBlockPos(ivec3 blockPos) {
     // Integer add/sub is 2x faster on Nvidia GPUs
     #ifdef INCLUDE_techniques_gi_PairwiseMISMetadata_glsl
-    return _voxel_spreadLUT[uint(blockPos.x)].x +
-        _voxel_spreadLUT[uint(blockPos.y)].y +
-        _voxel_spreadLUT[uint(blockPos.z)].z;
+    return _voxel_spreadLUTX[uint(blockPos.x)] +
+        _voxel_spreadLUTY[uint(blockPos.y)] +
+        _voxel_spreadLUTZ[uint(blockPos.z)];
     #else
     return _voxel_spreadLUT[uint(blockPos.x)] +
         (_voxel_spreadLUT[uint(blockPos.y)] << 1u) +
@@ -107,7 +109,9 @@ void voxel_initShared() {
     for (uint i = gl_LocalInvocationIndex; i < lutSize; i += localSize) {
         #ifdef INCLUDE_techniques_gi_PairwiseMISMetadata_glsl
         uint spreadBits = _voxel_spreadBits(i);
-        _voxel_spreadLUT[i] = uvec3(spreadBits, spreadBits << 1u, spreadBits << 2u);
+        _voxel_spreadLUTX[i] = spreadBits;
+        _voxel_spreadLUTY[i] = spreadBits << 1u;
+        _voxel_spreadLUTZ[i] = spreadBits << 2u;
         #else
         _voxel_spreadLUT[i] = _voxel_spreadBits(i);
         #endif
